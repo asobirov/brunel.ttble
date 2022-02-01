0f804abf8bef4110be9a2564d185e52532febe82
Akbarshokh Sobirov <asobirov357@gmail.com>
2022-02-01 15:02:26 +0000

    feat: add new Page links

--- src/types/pages.ts
+++ src/types/pages.ts
@@ -2,5 +2,6 @@ export enum Pages {
     login = "https://login.navigate.navitas.com",
     home = 'https://axis.navitas.com/',
     timetable = 'https://axis.navitas.com/apps/timetable',
-    moodle = 'https://moodle.libt.navitas.com/my/'
+    moodleMain = 'https://moodle.libt.navitas.com/my/',
+    course = 'https://moodle.libt.navitas.com/course/view.php?id=',
 }
