aad232059e62e32690fa974c34e044bc5bdc88fa
Akbarshokh Sobirov <asobirov357@gmail.com>
2022-02-09 18:20:01 +0000

    fix: fix  timtable write

--- src/helpers/write.ts
+++ src/helpers/write.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 import { LogType } from '../types/log';
 import { Course } from "../types/course";
 import { log } from './log';
+import path from 'path';
 
 export const writeTtble = async (data: Course[]) => {
     const json = JSON.stringify(data, null, 2);
@@ -9,28 +10,34 @@ export const writeTtble = async (data: Course[]) => {
     // console.log(json);
     log('Saving the provided timetable...', LogType.blockStart);
 
-    const fd = fs.openSync(__dirname + '/../../data/ttble.json', 'w');
+    // const fd = fs.openSync(__dirname + '/../../data/ttble.json', 'w');
 
-    const existingData = fs.readFileSync(__dirname + '/../../data/ttble.json', 'utf8');
+    // read ttble.json data
+    const existingData = fs.readFileSync(path.resolve(__dirname, '../../data/ttble.json'), 'utf8');
     if (existingData === json) {
         log('Finished saving. No changes were made to the timetable data.', LogType.end);
         return;
     }
 
     if (existingData) {
-        const existingDataJSON = JSON.parse(existingData.toString());
-        let newData: Course[] = [];
+        log('Found existing timetable data. Merging...', LogType.start);
+        const existingDataJSON = JSON.parse(existingData)
+        let filteredData: Course[] = [...data];
 
         existingDataJSON.forEach((c: Course) => {
-            if (!data.find(d => d.start === c.start)) {
-                newData.push(c);
+            if (data.find(d => d.start === c.start)) {
+                console.log('Found existing course with the same start time.');
+                return;
             }
+            filteredData.push(c);
         });
-        fs.writeFileSync(__dirname + '/../../data/ttble.json', JSON.stringify(newData, null, 2));
+
+        const mergedData = [...filteredData, ...data];
+        fs.writeFileSync(__dirname + '/../../data/ttble.json', JSON.stringify(mergedData, null, 2));
     } else {
         fs.writeFileSync(__dirname + '/../../data/ttble.json', json);
     }
-    fs.closeSync(fd);
+    // fs.closeSync(fd);
     log('Finished saving timetable.', LogType.end);
 }
 
